cc84db19be5dbf737bc06d81a38c067754f93229
Wilhelm Burger <wilbur@ieee.org>
2017-08-28 16:26:05 +0200

    redundant dependencies removed

--- src/main/java/imagingbook_test/TestClass.java
+++ src/main/java/imagingbook_test/TestClass.java
@@ -1,14 +1,16 @@
-package trump;
+package imagingbook_test;
 
+import ij.ImageJ;
 import imagingbook.lib.settings.PrintPrecision;
 
 public class TestClass {
 	
 	public static void main(String[] args) {
 		int pp = PrintPrecision.get();
-		System.out.println("Hello. " + pp);
+		System.out.println("PrintPrecision (imagingbook) = " + pp);
 		
-
+		System.out.println("ImageJ version = " + ImageJ.VERSION);
+		new ImageJ();
 	}
 
 }
